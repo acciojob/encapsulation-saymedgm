b8fe0445798f3651a50cd4569375a40cb59eb35c
saymedgm <deepak.cse.dtu@gmail.com>
2024-02-27 16:17:49 +0530

    1

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+
+    // Getter
+    public String getName() {
+        return name;
+    }
+
+    // Setter
+    public void setName(String name) {
+        this.name = name;
+    }
 }
